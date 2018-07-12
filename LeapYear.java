c9d48e6b61b18ee46b92b5707086ae285b8907cc
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-12 22:44:12 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -9,7 +9,7 @@ public class LeapYear {
 			if((year%400==0)&&(year%4==0)&&(year%100)==0){
 				System.out.println("YES");
 			}
-			else if((year%400==0)&&(year%4==0)||(year%100)!=0){
+			else if((year%400==0)||(year%4==0)&&(year%100)!=0){
 				System.out.println("YES");	
 			}	else{
 				System.out.println("NO");
