e15deac227635b6edffd4765a6cede37dc6f2476
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-12 22:54:34 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,12 +3,10 @@ import java.util.*;
 public class hello {
 	public static void main(String args[]){
 	int n;
-	String name;
+	String name="hello";
 	System.out.println("enter the Number");
 	Scanner in=new Scanner(System.in);
 	n=in.nextInt();
-	System.out.println("enter the String");
-	name=in.next();
 	while(n!=0){
 		System.out.println(name);
 		n--;
