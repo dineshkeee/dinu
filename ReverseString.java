da8e21c6a22c4c1c5ffd186142b16a69d9b53965
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-12 18:21:50 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,16 @@
 package setrep;
-
+import java.util.*;
 public class ReverseString {
-
+	public static void main(String args[]){
+		int i;
+		String b="",a;
+        System.out.println("Enter the String");
+        Scanner in=new Scanner(System.in);
+       a=in.next();
+        for( i=a.length()-1;i>=0;i--){
+        	b=b+(a.charAt(i));
+        }
+        System.out.println(b);
+	}
 }
+
