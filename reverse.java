a300edd72a167ef08a5786cbad1c663dc59fcc23
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-12 17:57:26 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,20 @@
+package setrep;
+import java.util.*;
+public class reverse {
+	public static void main(String args[]){
+		int a,i,j;
+		String b="";
+		System.out.println("Enter the Array Length");
+		Scanner in=new Scanner(System.in);
+        a=in.nextInt();
+        System.out.println("Enter the array Numbers");
+        int arr[]=new int[a];
+        for( i=0;i<arr.length;i++){
+        	arr[i]=in.nextInt();
+        }
+        for( i=arr.length-1;i>=0;i--){
+        	b=b+arr[i];
+        }  
+        System.out.println(b);
+	}
+}
