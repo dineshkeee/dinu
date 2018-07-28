5ffb13ee4677c4c78d87fc0cd8d0972bee82443c
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-28 17:54:39 +0530

    Add files via upload

---
+++ mathPow.java
@@ -0,0 +1,11 @@
+package set2;
+import java.util.*;
+public class mathPow {
+     public static void main(String args[]){
+    	 int a,b;
+    	 Scanner in=new Scanner(System.in);
+    	 a=in.nextInt();
+    	 b=in.nextInt();
+    	 System.out.println((int)Math.pow(a,b));
+     }
+}
