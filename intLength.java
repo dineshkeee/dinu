64ab1846c46b4d761d9afd2d0fa0bc80a7b8c622
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-28 17:44:34 +0530

    Add files via upload

---
+++ intLength.java
@@ -0,0 +1,12 @@
+package set1;
+import java.util.*;
+public class intLength {
+	public static void  main(String args[]){
+		int a;
+		String b;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=Integer.toString(a);
+		System.out.println(b.length());
+	}
+}
