5b46d147466b2352b6c8addf40eafeed2bc720ba
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 10:07:03 +0530

    Add files via upload

---
+++ multipleInt.java
@@ -0,0 +1,12 @@
+package set2;
+import java.util.*;
+public class multipleInt {
+	public static void main(String args[]){
+		int a,i;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		for(i=1;i<=5;i++){
+			System.out.print((a*i)+" ");
+		}
+	}
+}
